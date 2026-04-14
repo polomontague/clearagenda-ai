d62312d385851b29f4ec8b6dd489097f897ca7e9
Polo Montague <leopoldmontague@Polos-MacBook-Pro.local>
2026-04-14 02:57:25 -0400

    created update item endpoint at PUT /api/v1/items/[item_id]

--- app/api/v1/items/[item_id]/route.ts
+++ app/api/v1/items/[item_id]/route.ts
@@ -1,11 +1,47 @@
 import ItemsDAO from "@/dao/ItemsDAO"
+import AI from "@/lib/AI"
 import Auth from "@/lib/Auth"
 import Error from "@/lib/Error"
 import HttpError from "@/lib/HttpError"
 import Request from "@/lib/Request"
 import Response from "@/lib/Response"
-import { itemParamsSchema } from "@/schemas/items"
+import { itemBodySchema, itemParamsSchema } from "@/schemas/items"
 import { NextRequest } from "next/server"
+
+export const PUT = async (req: NextRequest, props: { params: Promise<{ item_id: string }> }) => {
+    try {
+        const user = await Auth.authenticate(req)
+        const params = await Request.params(props, itemParamsSchema)
+        const body = await Request.body(req, itemBodySchema)
+        const foundItem = await ItemsDAO.getItemById(params.item_id)
+        if (!foundItem) throw new HttpError(Response.notFound())
+        if (user.id !== foundItem.user.id) throw new HttpError(Response.forbidden())
+        // Only use AI again if description has changed
+        const data = body.description !== foundItem.description ? await AI.plan(body.description) : {
+            name: foundItem.name,
+            steps: foundItem.steps,
+            importance: foundItem.importance
+        }
+
+        const item = await ItemsDAO.updateItem(params.item_id, {
+            name: data.name,
+            description: body.description,
+            steps: data.steps.map(step => ({
+                name: step.name,
+                notes: step.notes,
+                duration: step.duration
+            })),
+            deadline: body.deadline,
+            importance: data.importance
+        })
+
+        return Response.ok({ item })
+    } catch (err) {
+        if (err instanceof HttpError) return err.response
+        Error.notify(err)
+        return Response.internalServerError()
+    }
+}
 
 export const DELETE = async (req: NextRequest, props: { params: Promise<{ item_id: string }> }) => {
     try {
