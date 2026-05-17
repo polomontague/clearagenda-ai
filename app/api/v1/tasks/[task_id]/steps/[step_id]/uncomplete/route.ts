3a68faf706e64b43abbebf9a5f56c3e17d1a9ef0
Polo Montague <leopoldmontague@gmail.com>
2026-05-17 02:54:30 -0400

    created uncomplete step endpoint

--- app/api/v1/tasks/[task_id]/steps/[step_id]/uncomplete/route.ts
+++ app/api/v1/tasks/[task_id]/steps/[step_id]/uncomplete/route.ts
@@ -1,12 +1,13 @@
-import Auth from "@/lib/Auth"
-import Error from "@/lib/Error"
+import { NextRequest } from "next/server"
 import HttpError from "@/lib/HttpError"
 import Response from "@/lib/Response"
-import { NextRequest } from "next/server"
+import Error from "@/lib/Error"
+import Auth from "@/lib/Auth"
+import { stepParamsSchema } from "@/schemas/tasks"
 import Request from "@/lib/Request"
-import { stepBodySchema, stepParamsSchema } from "@/schemas/tasks"
+import { stepBodySchema } from "@/schemas/tasks"
 import TasksDAO from "@/dao/TasksDAO"
-import { OnceStep, RepeatingStep } from "@/types/Task"
+import { RepeatingStep } from "@/types/Task"
 
 export const POST = async (req: NextRequest, props: { params: Promise<{ task_id: string, step_id: string }> }) => {
     try {
@@ -19,19 +20,15 @@ export const POST = async (req: NextRequest, props: { params: Promise<{ task_id:
         if (!step) throw new HttpError(Response.childNotFound("Step Does Not Exist"))
         if (task.occurs === "repeating" && !body.date) throw new HttpError(Response.validationError({ date: "date must be a string" }))
         if (user.id !== task.user.id) throw new HttpError(Response.forbidden())
-
+        
         if (task.occurs == "once") {
-            let completed = (step as OnceStep).completed
-            if (!completed) completed = await TasksDAO.updateStepCompleted(params.step_id, new Date().toISOString())
-            return Response.ok({ completed })
+            await TasksDAO.updateStepCompleted(params.step_id, undefined)
+            return Response.ok()
         }
         if (task.occurs === "repeating" && body.date) {
-            let completion = (step as RepeatingStep).completions.find(completion => completion.date === body.date)
-            if (!completion) completion = await TasksDAO.createStepCompletion(params.step_id, {
-                date: body.date,
-                completed: new Date().toISOString()
-            })
-            return Response.ok({ completion })
+            const completion = (step as RepeatingStep).completions.find(completion => completion.date === body.date)
+            if (completion) await TasksDAO.deleteStepCompletion(completion.id)
+            return Response.ok()
         }
     } catch (err) {
         if (err instanceof HttpError) return err.response
